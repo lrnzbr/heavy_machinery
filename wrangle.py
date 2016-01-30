a3588216e8cbf558c2c02ed49f1b92b098a6ede8
Francisco Cervera <fcocervera@users.noreply.github.com>
2016-01-29 17:14:00 -0800

    Update wrangle.py

--- wrangle.py
+++ wrangle.py
@@ -1,24 +1,22 @@
-
 import pandas as pd
 import numpy as np
 import scipy.stats as scs
 from dateutil import parser
-​
+
 def drop_columns(df, names):
 	df.drop(names, axis =1, inplace=True)
-​
+
 def stanardize_YearMade(df):
 	df['YearMade'] = df['YearMade'].apply(lambda x: 0 if x < 1970 else x - 1970)
-​
+
 def strip_saledate(df):
 	df['saledate'] = df['saledate'].apply(parser.parse).map(lambda x: x.year)
-​
+
 def make_dummies(df, col_dummies):
 	dummies_i = pd.get_dummies(df, columns = col_dummies)
 	return dummies_i
-​
+
 if __name__ == '__main__':
-	## Correctly assigned mixed types and import.
 	dtype_dict = {	'fiModelSeries': np.object, 
 					'Coupler_System': np.object, 
 					'Grouser_Tracks': np.object, 
@@ -26,15 +24,13 @@ if __name__ == '__main__':
 					}
 	df = pd.read_csv('../data/Train.csv', dtype=dtype_dict)
 	y = df['SalePrice'].values
-​
-	## Get our X and y
+
 	first_drop = [	'SalesID', 'MachineID', 'ProductGroupDesc', 'datasource',
-					'fiModelDesc', 'fiSecondaryDesc', 'fiModelSeries', 
+					'fiModelDesc', 'fiSecondaryDesc', 'fiModelSeries', 'fiBaseModel',
 					'fiModelDescriptor', 'fiProductClassDesc', 'ModelID', 'SalePrice'
 					]
 	drop_columns(df, first_drop)
-​
-	## Data munging
+
 	stanardize_YearMade(df)
 	strip_saledate(df)
 	col_dummies = ['ProductSize', 'state', 'ProductGroup', 'Drive_System', 'Enclosure',
@@ -48,12 +44,9 @@ if __name__ == '__main__':
 				   'Travel_Controls', 'Differential_Type', 'Steering_Controls', 'auctioneerID'
 				   ]
 	clean_df = make_dummies(df, col_dummies)
-	clean_df.drop(col_dummies, axis = 1)
-​
+
+	mean_hour_median = clean_df['MachineHoursCurrentMeter'].mean()
+	clean_df['MachineHoursCurrentMeter'] = clean_df['MachineHoursCurrentMeter'].fillna(mean_hour_median)
+
 	X = clean_df.values
-    
-
-    
-    
-
 
