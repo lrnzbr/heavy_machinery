0a3eaef568beb10a9167bf19d9adbb8bb43f9c83
Francesco <francescociuci90@gmail.com>
2016-01-29 17:29:31 -0800

    Update model_phase1.py

--- model_phase1.py
+++ model_phase1.py
@@ -11,20 +11,36 @@ def fit_model(model, X, y, params=None):
 
 def predict_y(model, X):
     return model.predict(X)
-    
-def score_oob(model):
-    modelself.
 
 def data_frame_subset(X, y, n_subset):
-    row_selected = list(np.random.randint(0, df.shape[0],  num_rows))
+    row_selected = list(np.random.randint(0, X.shape[0],  n_subset))
     X_sub_set = X[row_selected, :]
     y_sub_set = y[row_selected]
     return X_sub_set, y_sub_set
 
-def pipeline(model, X, y, n_subset, params=None):
-    _x, _y = data_frame_subset(X,y,n_subset)  
+def pipeline(model, X, y, n_subset=None, params=None):
+    if n_subset:
+        _x, _y = data_frame_subset(X,y,n_subset)
+    else:
+        _x, _y =X, y
     X_train, X_test, y_train, y_test = train_test_split(_x,_y)
     fit_model(model, X_train, y_train, params)
-    mse_array = cross_val_score(model, X_test, y_test, cv=5, scoring='mean_squared_erroe')
+    mse_array = cross_val_score(model, X_test, y_test, cv=5, scoring='mean_squared_error')
     r2_array = cross_val_score(model, X_test, y_test, cv=5, scoring='r2')
     return np.mean(np.abs(mse_array)), np.mean(np.abs(r2_array))
+
+def create(y_predict):
+    y_dataframe = pd.dataframe(y_predict)
+    y_dataframe.to_csv()
+
+def feature_importance(model):
+    feature = model.feature_importance
+    np.arg_sort(feature)
+    
+if __name__=="__main__":
+
+    params = {'n_estimators': 500,
+                'max_feature': 30}
+    rf = RandomForestRegressor()
+    pipeline(rf, X, y)
+    #y_test = rf.predict(X_test)
